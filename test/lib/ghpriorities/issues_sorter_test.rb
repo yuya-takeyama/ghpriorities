f1e64e77c9517093f88ef2ca8c1c36d0cf5c6554
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2016-08-02 17:36:12 +0900

    Fix test

--- test/lib/ghpriorities/issues_sorter_test.rb
+++ test/lib/ghpriorities/issues_sorter_test.rb
@@ -1,3 +1,5 @@
+require 'ghpriorities/issues_sorter'
+
 class GHPriorities::IssuesSorterTest < Test::Unit::TestCase
   sub_test_case '.group_and_sort' do
     setup do
@@ -16,21 +18,19 @@ class GHPriorities::IssuesSorterTest < Test::Unit::TestCase
     end
 
     test 'group prioritized/unprioritized issues and sort them' do
-      issues = GHPriorities::IssuesSorter.group_and_sort(priorities: @priorities, issues: issues)
+      result = GHPriorities::IssuesSorter.group_and_sort(priorities: @priorities, issues: @issues)
 
-      assert do
-        issues == {
-          prioritized: [
-            {id: 5, title: 'Five'},
-            {id: 2, title: 'Two'},
-            {id: 1, title: 'One'},
-          ],
-          unprioritized: [
-            {id: 3, title: 'Three'},
-            {id: 4, title: 'Four'},
-          ],
-        }
-      end
+      assert_equal(result, {
+        prioritized: [
+          {id: 5, title: 'Five'},
+          {id: 2, title: 'Two'},
+          {id: 1, title: 'One'},
+        ],
+        unprioritized: [
+          {id: 4, title: 'Four'},
+          {id: 3, title: 'Three'},
+        ],
+      })
     end
   end
 end
