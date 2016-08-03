050a137d543ac9f3f74c296220149dc31eb32dcd
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2016-08-03 21:23:39 +0900

    Tweak style

--- app/assets/javascripts/components/issues.js.jsx
+++ app/assets/javascripts/components/issues.js.jsx
@@ -2,29 +2,27 @@ import { Component } from 'react';
 import { SortableContainer, SortableElement, arrayMove } from 'react-sortable-hoc';
 import axios from 'axios';
 import FontAwesome from 'react-fontawesome';
+import Ago from 'react-ago-component'
 
 const Issue = SortableElement(({ issue, issueState }) => {
-  let style = {};
+  let className, style = {};
 
-  switch (issueState) {
-    case 'OPEN':
-      if (issue.state !== 'open') {
-        style.display = 'none';
-      }
-      break;
+  const hidden = (issueState === 'OPEN' && issue.state !== 'open') ||
+    (issueState === 'CLOSED' && issue.state !== 'closed');
 
-    case 'CLOSED':
-      if (issue.state !== 'closed') {
-        style.display = 'none';
-      }
-      break;
+  if (hidden) {
+    style.display = 'none';
+    className = 'issue-container hidden';
+  } else {
+    className = 'issue-container';
   }
 
   return (
-    <li className='issue-container'>
+    <li className={ className }>
       <div style={ style }>
         <IssueState state={ issue.state } />
-        <div className="issue-title">{issue.title}</div>
+        <div className="issue-title"><a href={ issue.html_url }>{ issue.title }</a></div>
+        <IssueMisc issue={issue} />
       </div>
     </li>
   );
@@ -36,6 +34,18 @@ const IssueState = ({ state }) => {
   } else {
     return (<FontAwesome name='check-circle' className='issue-state issue-state-closed' />);
   }
+};
+
+const IssueMisc = ({ issue }) => {
+  return (
+    <div className="issue-misc">
+      #{issue.number}
+      {' '}
+      opened <Ago date={issue.created_at} />
+      {' '}
+      by <img src={issue.user.avatar_url} className="issue-user-avatar" /> {issue.user.login}
+    </div>
+  );
 };
 
 const IssuesList = SortableContainer(({ issues, issueState }) => {
@@ -70,7 +80,6 @@ export default class Issues extends Component {
           {' / '}
           <a href="#" onClick={this.showClosed.bind(this)}>Closed</a>
           {' / '}
-
           <a href="#" onClick={this.showAll.bind(this)}>All</a>
         </div>
         <IssuesList
